0b714155334c7262c39eb0e231c6d4dd6980298e
fatalistix <slavabal11@outlook.com>
2024-02-25 13:11:48 +0700

    changed to  static, added min and max borders

--- IGCPaint/src/main/java/ru/nsu/vbalashov2/igc/paint/tools/events/SizeEvent.java
+++ IGCPaint/src/main/java/ru/nsu/vbalashov2/igc/paint/tools/events/SizeEvent.java
@@ -1,7 +1,15 @@
 package ru.nsu.vbalashov2.igc.paint.tools.events;
 
 public record SizeEvent(int radius) {
-    public int initialRadius() {
+    public static int initialRadius() {
         return 10;
+    }
+
+    public static int minRadius() {
+        return 1;
+    }
+
+    public static int maxRadius() {
+        return 500;
     }
 }
