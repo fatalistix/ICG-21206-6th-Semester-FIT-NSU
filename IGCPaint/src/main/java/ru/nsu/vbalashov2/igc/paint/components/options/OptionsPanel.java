a2ffcf6b22f17ea2af0d5c1f61958fcaa09d0f07
fatalistix <slavabal11@outlook.com>
2024-02-25 13:41:56 +0700

    changed organization of components

--- IGCPaint/src/main/java/ru/nsu/vbalashov2/igc/paint/components/options/OptionsPanel.java
+++ IGCPaint/src/main/java/ru/nsu/vbalashov2/igc/paint/components/options/OptionsPanel.java
@@ -7,10 +7,16 @@ import java.awt.*;
 
 public class OptionsPanel extends JPanel {
     public OptionsPanel(EventBus eventBus) {
-        this.setLayout(new GridLayout(0, 1));
+//        this.setLayout(new GridLayout(0, 1));
+//
+//        this.add(new FormPanel(eventBus));
+//        this.add(new SizePanel(eventBus));
+//        this.add(new RotatePanel(eventBus));
 
-        this.add(new FormPanel(eventBus));
-        this.add(new SizePanel(eventBus));
-        this.add(new RotatePanel(eventBus));
+        this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+
+        this.add(new LabelsPanel());
+        this.add(new SlidersPanel(eventBus));
+        this.add(new SpinnersPanel(eventBus));
     }
 }
