76256a5d49da176a0cedfc5556c7863db2784e96
fatalistix <slavabal11@outlook.com>
2024-02-25 12:05:11 +0700

    added slider, values sinchronization and default values setting from FormEvent

--- IGCPaint/src/main/java/ru/nsu/vbalashov2/igc/paint/components/options/FormPanel.java
+++ IGCPaint/src/main/java/ru/nsu/vbalashov2/igc/paint/components/options/FormPanel.java
@@ -1,20 +1,52 @@
 package ru.nsu.vbalashov2.igc.paint.components.options;
 
 import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
 import ru.nsu.vbalashov2.igc.paint.tools.events.FormEvent;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
 import java.awt.*;
 
 public class FormPanel extends JPanel {
-    public FormPanel(EventBus eventBus) {
-        this.setLayout(new GridLayout(1, 2));
 
-        JLabel formLabel = new JLabel("Form:");
+    private final JSlider formIntSlider;
+    private final JSpinner formIntSpinner;
+
+    public FormPanel(EventBus eventBus) {
+        eventBus.register(this);
+
+        this.setLayout(new GridLayout(1, 3));
+
+        JLabel formLabel = new JLabel("Form (number of angles):");
         this.add(formLabel);
 
-        JSpinner formIntSpinner = new JSpinner(new SpinnerNumberModel(3, 3, 2000, 1));
+        formIntSlider = new JSlider(
+                FormEvent.minNumberOfAngles(),
+                FormEvent.maxNumberOfAngles(),
+                FormEvent.initialNumberOfAngles()
+        );
+        formIntSlider.setPaintLabels(true);
+        formIntSlider.setMajorTickSpacing(2);
+        formIntSlider.setMinorTickSpacing(1);
+        formIntSlider.addChangeListener((event) -> eventBus.post(new FormEvent(formIntSlider.getValue())));
+        this.add(formIntSlider);
+
+        formIntSpinner = new JSpinner(
+                new SpinnerNumberModel(
+                        FormEvent.initialNumberOfAngles(),
+                        FormEvent.minNumberOfAngles(),
+                        FormEvent.maxNumberOfAngles(),
+                        1
+                )
+        );
         formIntSpinner.addChangeListener((event) -> eventBus.post(new FormEvent((Integer) formIntSpinner.getValue())));
         this.add(formIntSpinner);
+    }
+
+    @Subscribe
+    private void handleFormEvent(FormEvent event) {
+        formIntSlider.setValue(event.numberOfAngles());
+        formIntSpinner.setValue(event.numberOfAngles());
     }
 }
