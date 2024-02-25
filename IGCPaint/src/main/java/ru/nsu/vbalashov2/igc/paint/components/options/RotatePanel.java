aa6311af1dfbf5d2edbd3fe2e6ff06e9adf0c959
fatalistix <slavabal11@outlook.com>
2024-02-25 13:09:13 +0700

    added slider, values sinchronization, default values from event

--- IGCPaint/src/main/java/ru/nsu/vbalashov2/igc/paint/components/options/RotatePanel.java
+++ IGCPaint/src/main/java/ru/nsu/vbalashov2/igc/paint/components/options/RotatePanel.java
@@ -1,25 +1,56 @@
 package ru.nsu.vbalashov2.igc.paint.components.options;
 
 import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import ru.nsu.vbalashov2.igc.paint.tools.events.FormEvent;
 import ru.nsu.vbalashov2.igc.paint.tools.events.RotateEvent;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class RotatePanel extends JPanel {
-    public RotatePanel(EventBus eventBus) {
-        this.setLayout(new GridLayout(1, 2));
 
-        JLabel rotateLabel = new JLabel("Rotation:");
+    private final JSlider rotateSlider;
+    private final JSpinner rotateIntSpinner;
+
+    public RotatePanel(EventBus eventBus) {
+        eventBus.register(this);
+
+        this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+
+        JLabel rotateLabel = new JLabel("Rotation (degrees):");
         this.add(rotateLabel);
 
-        JSpinner rotateIntSpinner = new JSpinner(
+        rotateSlider = new JSlider(
+                RotateEvent.minAngle(),
+                RotateEvent.maxAngle(),
+                RotateEvent.initialAngle()
+        );
+        rotateSlider.setPaintLabels(true);
+        rotateSlider.setMajorTickSpacing(20);
+        rotateSlider.setMinorTickSpacing(1);
+        rotateSlider.addChangeListener(
+                (event) -> eventBus.post(new RotateEvent(rotateSlider.getValue()))
+        );
+        this.add(rotateSlider);
+
+        rotateIntSpinner = new JSpinner(
                 new SpinnerNumberModel(
-                        0, 0, 360, 1
+                        RotateEvent.initialAngle(),
+                        RotateEvent.minAngle(),
+                        RotateEvent.maxAngle(),
+                        1
                 )
         );
         rotateIntSpinner.addChangeListener((event) -> eventBus.post(
                 new RotateEvent((Integer) rotateIntSpinner.getValue())
         ));
+        this.add(rotateIntSpinner);
+    }
+
+    @Subscribe
+    private void handleRotateEvent(RotateEvent rotateEvent) {
+        rotateSlider.setValue(rotateEvent.angle());
+        rotateIntSpinner.setValue(rotateEvent.angle());
     }
 }
