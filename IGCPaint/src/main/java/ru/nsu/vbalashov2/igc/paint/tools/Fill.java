e2f1dc2ae93bf7093961b273947e7f335af183ea
fatalistix <slavabal11@outlook.com>
2024-02-25 11:44:59 +0700

    fixed drawing bug, adopted to Tool interface

--- IGCPaint/src/main/java/ru/nsu/vbalashov2/igc/paint/tools/Fill.java
+++ IGCPaint/src/main/java/ru/nsu/vbalashov2/igc/paint/tools/Fill.java
@@ -1,48 +1,70 @@
 package ru.nsu.vbalashov2.igc.paint.tools;
 
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import ru.nsu.vbalashov2.igc.paint.tools.events.ColorEvent;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.Stack;
 
-public class Fill {
-    public static void fillArea(BufferedImage image, int x, int y, Color originalColor, Color fillColor) {
-        if (!Fill.inside(image, x, y, originalColor)) {
+public class Fill implements Tool {
+
+    private Color color = Color.BLACK;
+
+    public Fill(EventBus eventBus) {
+        eventBus.register(this);
+    }
+
+    @Override
+    public void use(BufferedImage image, int x, int y) {
+        int originalRGB = image.getRGB(x, y);
+        fillArea(image, x, y, originalRGB, color.getRGB());
+    }
+
+    private static void fillArea(BufferedImage image, int xStart, int yStart, int originalRGB, int fillRGB) {
+        if (!inside(image, xStart, yStart, originalRGB)) {
             return;
         }
-        Stack<Point> s = new Stack<>();
-        s.push(new Point(x, y));
-        while (!s.empty()) {
-            Point p = s.pop();
-            int lx = p.x;
-            while (Fill.inside(image, lx - 1, p.y, originalColor)) {
-                image.setRGB(lx - 1, y, fillColor.getRGB());
-                --lx;
+        Stack<Point> stack = new Stack<>();
+        stack.push(new Point(xStart, yStart));
+        while (!stack.empty()) {
+            Point spanPoint = stack.pop();
+            int leftX = spanPoint.x;
+            while (Fill.inside(image, leftX - 1, spanPoint.y, originalRGB)) {
+                image.setRGB(leftX - 1, spanPoint.y, fillRGB);
+                --leftX;
             }
-            int rx = p.x;
-            while (Fill.inside(image, rx, p.y, originalColor)) {
-                image.setRGB(rx, p.y, fillColor.getRGB());
-                ++rx;
+            int rightX = spanPoint.x;
+            while (Fill.inside(image, rightX, spanPoint.y, originalRGB)) {
+                image.setRGB(rightX, spanPoint.y, fillRGB);
+                ++rightX;
             }
-            Fill.scan(image, lx, rx - 1, p.y + 1, s, originalColor);
-            Fill.scan(image, lx, rx - 1, p.y - 1, s, originalColor);
+            Fill.scan(image, leftX, rightX - 1, spanPoint.y + 1, stack, originalRGB);
+            Fill.scan(image, leftX, rightX - 1, spanPoint.y - 1, stack, originalRGB);
         }
     }
 
-    private static boolean inside(BufferedImage image, int x, int y, Color originalColor) {
+    private static boolean inside(BufferedImage image, int x, int y, int originalRGB) {
         return 0 <= x && x < image.getWidth() &&
                 0 <= y && y < image.getHeight() &&
-                image.getRGB(x, y) == originalColor.getRGB();
+                image.getRGB(x, y) == originalRGB;
     }
 
-    private static void scan(BufferedImage image, int lx, int rx, int y, Stack<Point> stack, Color originalColor) {
+    private static void scan(BufferedImage image, int lx, int rx, int y, Stack<Point> stack, int originalRGB) {
         boolean spanAdded = false;
         for (int x = lx; x <= rx; ++x) {
-            if (!Fill.inside(image, x, y, originalColor)) {
+            if (!Fill.inside(image, x, y, originalRGB)) {
                 spanAdded = false;
             } else if (!spanAdded) {
-                stack.add(new Point(x, y));
+                stack.push(new Point(x, y));
                 spanAdded = true;
             }
         }
+    }
+
+    @Subscribe
+    private void handleColorEvent(ColorEvent colorEvent) {
+        color = colorEvent.color();
     }
 }
