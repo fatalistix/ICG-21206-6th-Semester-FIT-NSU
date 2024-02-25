a6025da7f0f899664e10ad2e674e0cd10ca23696
fatalistix <slavabal11@outlook.com>
2024-02-25 13:11:31 +0700

    changed to  static, added min and max borders

--- IGCPaint/src/main/java/ru/nsu/vbalashov2/igc/paint/tools/events/RotateEvent.java
+++ IGCPaint/src/main/java/ru/nsu/vbalashov2/igc/paint/tools/events/RotateEvent.java
@@ -1,7 +1,15 @@
 package ru.nsu.vbalashov2.igc.paint.tools.events;
 
 public record RotateEvent(int angle) {
-    public int initialAngle() {
+    public static int initialAngle() {
         return 0;
+    }
+
+    public static int minAngle() {
+        return 0;
+    }
+
+    public static int maxAngle() {
+        return 360;
     }
 }
