4bbb58440442319afe8e8a72d59519374d66ce43
fatalistix <slavabal11@outlook.com>
2024-02-25 13:09:44 +0700

    moved to ColorEvent

--- IGCPaint/src/main/java/ru/nsu/vbalashov2/igc/paint/components/toolbar/ColorButton.java
+++ IGCPaint/src/main/java/ru/nsu/vbalashov2/igc/paint/components/toolbar/ColorButton.java
@@ -1,6 +1,7 @@
 package ru.nsu.vbalashov2.igc.paint.components.toolbar;
 
 import com.google.common.eventbus.EventBus;
+import ru.nsu.vbalashov2.igc.paint.tools.events.ColorEvent;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,6 +9,6 @@ import java.awt.*;
 public class ColorButton extends JButton {
     public ColorButton(ImageIcon colorImageIcon, Color color, EventBus eventBus) {
         this.setIcon(colorImageIcon);
-        this.addActionListener((event) -> eventBus.post(color));
+        this.addActionListener((event) -> eventBus.post(new ColorEvent(color)));
     }
 }
