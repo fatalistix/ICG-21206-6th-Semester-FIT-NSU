83fe7d43d908c6fde33a47668fae2c736e0ae718
fatalistix <slavabal11@outlook.com>
2024-02-26 13:37:48 +0700

    added handling scroll events

--- IGCPaint/src/main/java/ru/nsu/vbalashov2/igc/paint/PaintFrame.java
+++ IGCPaint/src/main/java/ru/nsu/vbalashov2/igc/paint/PaintFrame.java
@@ -1,30 +1,54 @@
 package ru.nsu.vbalashov2.igc.paint;
 
 import com.google.common.eventbus.EventBus;
-import ru.nsu.vbalashov2.igc.paint.components.PaintMenuBar;
+import com.google.common.eventbus.Subscribe;
 import ru.nsu.vbalashov2.igc.paint.components.PaintPanel;
+import ru.nsu.vbalashov2.igc.paint.components.menubar.PaintMenuBar;
 import ru.nsu.vbalashov2.igc.paint.components.toolbar.PaintToolBar;
+import ru.nsu.vbalashov2.igc.paint.tools.events.ScrollRevalidateEvent;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 
 public class PaintFrame extends JFrame {
-    private static final Color INITIAL_COLOR = Color.BLACK;
+
+    private final JScrollPane scrollPane;
+
     public PaintFrame() throws IOException {
         super("IGCPaint");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setMinimumSize(new Dimension(640, 480));
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        setMinimumSize(new Dimension(640, 480));
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int result = JOptionPane.showConfirmDialog(
+                        PaintFrame.this,
+                        "Are you sure you want to exit? All unsaved data will be lost.",
+                        "Exit?",
+                        JOptionPane.YES_NO_OPTION
+                );
+
+                if (result == JOptionPane.OK_OPTION) {
+                    System.exit(0);
+                }
+            }
+        });
 
         var eventBus = new EventBus();
 
-        var paintMenuBar = new PaintMenuBar();
+        eventBus.register(this);
+
+        var paintMenuBar = new PaintMenuBar(eventBus);
         this.setJMenuBar(paintMenuBar);
 
-        PaintToolBar paintToolBar = new PaintToolBar(INITIAL_COLOR, eventBus);
+        PaintToolBar paintToolBar = new PaintToolBar(eventBus);
 
         PaintPanel paintPanel = new PaintPanel(eventBus);
-        JScrollPane scrollPane = new JScrollPane(paintPanel);
+        scrollPane = new JScrollPane(paintPanel);
 
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
@@ -33,5 +57,11 @@ public class PaintFrame extends JFrame {
         this.getContentPane().add(scrollPane);
 
         this.setVisible(true);
+    }
+
+    @Subscribe
+    private void handleScrollRevalidateEvent(ScrollRevalidateEvent event) {
+        scrollPane.revalidate();
+        scrollPane.updateUI();
     }
 }
