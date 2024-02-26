9a941f1d6183695ec77bc2d143946af188fbc474
fatalistix <slavabal11@outlook.com>
2024-02-26 13:37:35 +0700

    removed 'optimization'

--- IGCPaint/src/main/java/ru/nsu/vbalashov2/igc/paint/Main.java
+++ IGCPaint/src/main/java/ru/nsu/vbalashov2/igc/paint/Main.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("sun.java2d.opengl", "true");
         SwingUtilities.invokeLater(() -> {
             try {
                 new PaintFrame();
