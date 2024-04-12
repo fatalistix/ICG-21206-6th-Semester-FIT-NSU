8294493b44632f7067d9ae58baabf5566d387e69
fatalistix <slavabal11@outlook.com>
2024-04-12 10:18:34 +0700

    added all menus

--- ICGFilter/src/main/java/ru/nsu/vbalashov2/icg/filter/components/menubar/FilterMenuBar.java
+++ ICGFilter/src/main/java/ru/nsu/vbalashov2/icg/filter/components/menubar/FilterMenuBar.java
@@ -1,7 +1,19 @@
 package ru.nsu.vbalashov2.icg.filter.components.menubar;
 
+import ru.nsu.vbalashov2.icg.filter.components.menubar.aboutmenu.AboutMenu;
+import ru.nsu.vbalashov2.icg.filter.components.menubar.filemenu.FileMenu;
+import ru.nsu.vbalashov2.icg.filter.components.menubar.filtermenu.FilterMenu;
+import ru.nsu.vbalashov2.icg.filter.components.menubar.viewmenu.ViewMenu;
+import ru.nsu.vbalashov2.icg.filter.tools.events.EventPublishSubjects;
+
 import javax.swing.*;
 
 public class FilterMenuBar extends JMenuBar {
 
+    public FilterMenuBar(EventPublishSubjects eventPublishSubjects) {
+        add(new FileMenu(eventPublishSubjects));
+        add(new FilterMenu(eventPublishSubjects));
+        add(new ViewMenu(eventPublishSubjects));
+        add(new AboutMenu());
+    }
 }
