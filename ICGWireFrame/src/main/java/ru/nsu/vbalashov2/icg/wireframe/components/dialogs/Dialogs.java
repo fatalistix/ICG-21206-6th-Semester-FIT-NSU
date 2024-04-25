4cb07756da57abf580ecf5ab39f5c99db9b15050
fatalistix <slavabal11@outlook.com>
2024-04-25 11:01:18 +0700

    utils class with creation dialogs

---
+++ ICGWireFrame/src/main/java/ru/nsu/vbalashov2/icg/wireframe/components/dialogs/Dialogs.java
@@ -0,0 +1,20 @@
+package ru.nsu.vbalashov2.icg.wireframe.components.dialogs;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Dialogs {
+
+    public static void showExitDialog(Component parentComponent) {
+        int result = JOptionPane.showConfirmDialog(
+                parentComponent,
+                "Are you sure you want to exit? All unsaved data will be lost.",
+                "Exit?",
+                JOptionPane.YES_NO_OPTION
+        );
+
+        if (result == JOptionPane.OK_OPTION) {
+            System.exit(0);
+        }
+    }
+}
