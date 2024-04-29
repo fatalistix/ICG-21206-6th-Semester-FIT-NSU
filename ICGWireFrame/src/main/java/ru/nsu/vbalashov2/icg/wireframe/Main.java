068515197a1ca3c09c0def20370dc3216dc4cd1f
fatalistix <slavabal11@outlook.com>
2024-04-29 14:48:56 +0700

    possible optimisation for swing

--- ICGWireFrame/src/main/java/ru/nsu/vbalashov2/icg/wireframe/Main.java
+++ ICGWireFrame/src/main/java/ru/nsu/vbalashov2/icg/wireframe/Main.java
@@ -2,6 +2,7 @@ package ru.nsu.vbalashov2.icg.wireframe;
 
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("sun.java2d.enableAccelerated2D", "true");
         new WireframeFrame().start();
     }
 }
