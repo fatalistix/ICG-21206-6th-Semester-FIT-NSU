cd30055e833dd4fa0a7851263ed66b3a0e95ea06
fatalistix <slavabal11@outlook.com>
2024-04-29 15:03:09 +0700

    renamed anchor points ps, added reset rotation event ps

--- ICGWireFrame/src/main/java/ru/nsu/vbalashov2/icg/wireframe/tools/events/EventPublishSubjects.java
+++ ICGWireFrame/src/main/java/ru/nsu/vbalashov2/icg/wireframe/tools/events/EventPublishSubjects.java
@@ -3,18 +3,20 @@ package ru.nsu.vbalashov2.icg.wireframe.tools.events;
 import io.reactivex.rxjava3.subjects.PublishSubject;
 import lombok.Getter;
 
+import java.util.List;
 import java.awt.*;
 import java.io.File;
-import java.util.List;
 
 @Getter
 public class EventPublishSubjects {
 
-    private final PublishSubject<List<Point>> bSplineAnchorPointsPublishSubject = PublishSubject.create();
+    private final PublishSubject<List<Point>> bSplineAnchorPointsEventPublishSubject = PublishSubject.create();
     private final PublishSubject<Integer> bSplineNPublishSubject = PublishSubject.create();
     private final PublishSubject<Integer> bSplineMPublishSubject = PublishSubject.create();
     private final PublishSubject<Integer> bSplineM1PublishSubject = PublishSubject.create();
 
     private final PublishSubject<File> saveFilePublishSubject = PublishSubject.create();
     private final PublishSubject<File> loadFilePublishSubject = PublishSubject.create();
+
+    private final PublishSubject<ResetWireframeRotationEvent> resetWireframeRotationEventPublishSubject = PublishSubject.create();
 }
